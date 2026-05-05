046834bef8366b319963eb1c319bf4451b668d7c
manahilsajjad <manahilsajjad2014@gmail.com>
2026-05-05 18:14:39 +0200

    Delete intersection.py

--- intersection.py
+++
@@ -1,8 +0,0 @@
-setx={"green", "blue"}
-sety = {"blue","yellow"}
-print("Original set elements:")
-print(setx)
-print(sety)
-print("\nIntersection of two said sets:")
-set3 = setx.intersection(sety)
-print(set3)
